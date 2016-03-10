350fc7a6776631f1961f095e77866f5bde986762
Vannevelj <jer_vannevel@hotmail.com>
2016-03-10 18:28:32 +0100

    init import

---
+++ RedditCrap/app/js/popup.js
@@ -0,0 +1,110 @@
+var shittyCrapSites = ['dailymail.co.uk', 'independent.co.uk', 'ibtimes.co.uk', 'huffingtonpost.com', 'rt.com', 'express.co.uk', 'telegraph.co.uk'];
+
+// We seed the storage with crappy sites if none are found yet
+chrome.storage.sync.get(['crappySites', 'crappyAction'], function (data) {
+    if (!data.crappySites || data.crappySites.length === 0) {
+        chrome.storage.sync.set({ 'crappySites': shittyCrapSites }, function () {});
+    }
+
+    var selectElement = document.getElementById('crappyAction');
+    if (!data.crappyAction) {
+        chrome.storage.sync.set({ 'crappyAction': 1 }, function () {});
+    } else {
+        selectElement.selectedIndex = data.crappyAction;
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    displayExistingFilters();
+
+    document.getElementById('siteForm').addEventListener('submit', add);
+    document.getElementById('reset').addEventListener('click', reset);
+    document.getElementById('crappyAction').addEventListener('change', actionSettingChanged);
+});
+
+function displayExistingFilters() {
+    // Get the existing filters from storage
+    chrome.storage.sync.get('crappySites', function (data) {
+        // Add them to the list
+        data.crappySites.forEach(function (element) {
+            addListElement(element);
+        }, this);
+    });
+}
+
+function addListElement(value) {
+    if (!value) {
+        return;
+    }
+
+    var resultList = document.getElementById('crappySites');
+    var inputField = document.getElementById('siteInput');
+
+    var newItem = document.createElement('li');
+    newItem.setAttribute('id', value);
+    newItem.appendChild(document.createTextNode(value + ' '));
+
+    var newCancelImage = document.createElement('img');
+    newCancelImage.setAttribute('src', '../res/cross.png');
+    newCancelImage.addEventListener('click', function () { remove(value); });
+    newItem.appendChild(newCancelImage);
+
+    resultList.appendChild(newItem);
+    inputField.value = '';
+}
+
+function remove(value) {
+    chrome.storage.sync.get('crappySites', function (data) {
+        var sites = data.crappySites;
+        console.log('trying to remove ' + value);
+
+        var newSites = [];
+        for (var i = 0; i < sites.length; i++) {
+            if (sites[i] !== value) {
+                newSites.push(sites[i]);
+            }
+        }
+
+        console.log('new list: ' + newSites);
+
+        chrome.storage.sync.set({ 'crappySites': newSites }, function () {});
+
+        var resultList = document.getElementById('crappySites');
+        resultList.removeChild(document.getElementById(value));
+    });
+}
+
+function add(e) {
+    e.preventDefault();
+
+    var inputField = document.getElementById('siteInput');
+    var value = inputField.value;
+    addListElement(value);
+
+    chrome.storage.sync.get('crappySites', function (data) {
+        var sites = data.crappySites;
+        sites.push(value);
+        chrome.storage.sync.set({ 'crappySites': sites }, function () {});
+    });
+}
+
+function reset() {
+    chrome.storage.sync.set({ 'crappySites': shittyCrapSites }, function () {});
+
+    var resultList = document.getElementById('crappySites');
+    var newResultList = document.createElement('ul');
+    newResultList.setAttribute('id', 'crappySites');
+
+    resultList.parentElement.replaceChild(newResultList, resultList);
+
+    shittyCrapSites.forEach(function (element) {
+        addListElement(element);
+    }, this);
+}
+
+function actionSettingChanged() {
+    var selectElement = document.getElementById('crappyAction');
+    var selectedOption = selectElement.selectedIndex;
+
+    chrome.storage.sync.set({ 'crappyAction': selectedOption }, function () {});
+}
---
+++ RedditCrap/app/js/redditcrap.js
@@ -0,0 +1,98 @@
+// Register an event listener to check for crap when the stored sites change
+chrome.storage.onChanged.addListener(function(changes, area) {
+    checkCrap();
+})
+
+var counter = 1;
+// Finds all the appropriate domains in the DOM, checks whether they're blacklisted and acts accordingly
+function checkCrap() {
+    var domains = document.getElementsByClassName('domain');
+    chrome.storage.sync.get(['crappySites', 'crappyAction'], function(data) {
+        for (var i = 0; i < domains.length; i++) {
+            // urls are in the form of '(url)'
+            // (surrounded by brackets)
+            var url = domains[i].textContent.substring(1, domains[i].textContent.length - 1);
+
+            var parentNode = getParentByClass(domains[i], 'title');
+            var entryNode = getThingNode(parentNode);
+            if (parentNode.style.backgroundColor === 'red' ||
+                entryNode.style.display === 'none') {
+
+                parentNode.style.backgroundColor = '';
+                entryNode.style.display = '';
+            }
+
+            for (var index = 0; index < data.crappySites.length; index++) {
+                var shouldBeColoured = data.crappySites[index].indexOf(url) > -1;
+                if (shouldBeColoured) {
+                    if (data.crappyAction === 1) {
+                        parentNode.style.backgroundColor = 'red';
+                    }
+
+                    if (data.crappyAction === 2) {
+                        entryNode.style.display = 'none';
+                    }
+
+                    break;
+                }
+            }
+        }
+    });
+}
+
+function getParentByClass(currentNode, className) {
+    while (currentNode = currentNode.parentElement) {
+        if (currentNode.classList.contains(className)) {
+            return currentNode;
+        }
+    }
+}
+
+function getThingNode(parentNode) {
+    while (parentNode = parentNode.parentElement) {
+        if (parentNode.getAttribute('data-fullname')) {
+            return parentNode;
+        }
+    }
+}
+
+// In this section we perform crapchecks when the page has changed
+// This is important in the case of NeverEndingReddit which allows a user to scroll down to load a new page
+// By introducing a short delay we prevent the page from being crapchecked for every modification (which is problematic if they come in quick succession)
+var lastTimeModified;
+var timeDelay = 2000; // 2 seconds
+
+// When the subtree is modified for the first time we do an initial crapcheck.
+// This allows us to look responsive while we discard all but one closely subsequent events
+// After the last event we call checkCrap() again to make sure nothing has been missed due to the subsequent modification events
+// Afterwards we reset the value so this can be repeated for other grouped subtree modification events
+var performedFastCheck = false;
+
+document.addEventListener('DOMSubtreeModified', function() {
+    if (!performedFastCheck) {
+        checkCrap();
+        performedFastCheck = true;
+    }
+
+    lastTimeModified = new Date().getTime();
+
+    setTimeout(function() {
+        startTimer();
+    }, timeDelay);
+});
+
+function startTimer() {
+    if (lastTimeModified + timeDelay > new Date().getTime()) {
+        clearTimeout();
+        setTimeout(function() {
+            startTimer()
+        }, timeDelay);
+    } else {
+        checkCrap();
+        performedFastCheck = false;
+    }
+};
+
+
+// TODO: allowing users to give strikes against certain sources
+// imgur.com + i.imgur.com
